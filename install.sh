84b974b81776cac70553e10c48d553a30cbcfc34
paskalsq <ilyatop12345@yandex.ru>
2024-09-15 10:46:43 +0300

    It works?

--- install.sh
+++ install.sh
@@ -10,6 +10,17 @@ fi
 echo "Установка необходимых пакетов..."
 sudo pacman -Syu --noconfirm git zsh neovim hyprland alacritty dunst rofi waybar # Добавь сюда нужные тебе пакеты
 
+# Запрос на установку дополнительных пакетов
+read -p "Введите дополнительные пакеты через пробел (или нажмите Enter, чтобы пропустить): " extra_packages
+
+# Установка дополнительных пакетов, если они введены
+if [ -n "$extra_packages" ]; then
+  echo "Установка дополнительных пакетов: $extra_packages"
+  sudo pacman -S --noconfirm $extra_packages
+else
+  echo "Дополнительные пакеты не указаны, пропускаем..."
+fi
+
 # Клонирование dotfiles из репозитория
 echo "Клонирование dotfiles..."
 if [ -d "$HOME/dotfiles" ]; then
@@ -22,7 +33,13 @@ fi
 # Симлинк конфигурационных файлов
 echo "Создание символических ссылок..."
 ln -sf "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
-ln -sf "$HOME/dotfiles/.config/nvim" "$HOME/.config/nvim"
+ln -sf "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
+ln -sf "$HOME/dotfiles/hypr" "$HOME/.config/hypr"
+ln -sf "$HOME/dotfiles/alacritty/" "$HOME/.config/alacritty"
+ln -sf "$HOME/dotfiles/dunst" "$HOME/.config/dunst"
+ln -sf "$HOME/dotfiles/rofi" "$HOME/.config/rofi"
+ln -sf "$HOME/dotfiles/waybar" "$HOME/.config/waybar"
+
 # Добавь сюда другие конфигурационные файлы
 
 # Установка Zsh в качестве основной оболочки
