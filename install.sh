92e82e8dd372d425231bdb336db2ed5e62f25b19
paskalsq <ilyatop12345@yandex.ru>
2024-09-15 14:48:02 +0300

    It works?

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# Проверка на root-права
+# Проверка на root-права для установки пакетов
 if [ "$EUID" -ne 0 ]; then
-  echo "Пожалуйста, запустите скрипт с правами root"
-  exit
+  echo "Установка пакетов требует прав root, запрашиваю sudo..."
 fi
 
-# Установка необходимых пакетов
-echo "Установка необходимых пакетов..."
-sudo pacman -Syu --noconfirm git zsh neovim hyprland alacritty dunst rofi waybar # Добавь сюда нужные тебе пакеты
+# Установка обязательных пакетов
+echo "Установка обязательных пакетов..."
+sudo pacman -Syu --noconfirm git zsh neovim hyprland alacritty dunst rofi waybar eza
 
 # Запрос на установку дополнительных пакетов
 read -p "Введите дополнительные пакеты через пробел (или нажмите Enter, чтобы пропустить): " extra_packages
@@ -22,27 +21,36 @@ else
 fi
 
 # Клонирование dotfiles из репозитория
-echo "Клонирование dotfiles..."
+echo "Клонирование dotfiles в домашнюю директорию..."
 if [ -d "$HOME/dotfiles" ]; then
   echo "Папка dotfiles уже существует, обновляю..."
   cd "$HOME/dotfiles" && git pull
 else
-  git clone https://github.com/paskalsq/dotfiles.git "$HOME/dotfiles"
+  git clone https://github.com/your_username/dotfiles.git "$HOME/dotfiles"
 fi
+
+# Создание необходимых директорий
+echo "Создание необходимых директорий..."
+mkdir -p $HOME/.config/nvim/
+mkdir -p $HOME/.config/hypr/
+mkdir -p $HOME/.config/alacritty/
+mkdir -p $HOME/.config/dunst/
+mkdir -p $HOME/.config/rofi/
+mkdir -p $HOME/.config/waybar/
+mkdir -p $HOME/.config/gtk-3.0/
+mkdir -p $HOME/.config/gtk-4.0/
 
 # Симлинк конфигурационных файлов
 echo "Создание символических ссылок..."
 ln -sf "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
 ln -sf "$HOME/dotfiles/nvim" "$HOME/.config/nvim"
 ln -sf "$HOME/dotfiles/hypr" "$HOME/.config/hypr"
-ln -sf "$HOME/dotfiles/alacritty/" "$HOME/.config/alacritty"
+ln -sf "$HOME/dotfiles/alacritty" "$HOME/.config/alacritty"
 ln -sf "$HOME/dotfiles/dunst" "$HOME/.config/dunst"
 ln -sf "$HOME/dotfiles/rofi" "$HOME/.config/rofi"
 ln -sf "$HOME/dotfiles/waybar" "$HOME/.config/waybar"
-ln -sf "$HOME/dotfiles/gtk-3.0/" "$HOME/.config/gtk-3.0/"
-ln -sf "$HOME/dotfiles/gtk-4.0/" "$HOME/.config/gtk-4.0/"
-
-# Добавь сюда другие конфигурационные файлы
+ln -sf "$HOME/dotfiles/gtk-3.0" "$HOME/.config/gtk-3.0"
+ln -sf "$HOME/dotfiles/gtk-4.0" "$HOME/.config/gtk-4.0"
 
 # Установка Zsh в качестве основной оболочки
 echo "Установка Zsh в качестве основной оболочки..."
