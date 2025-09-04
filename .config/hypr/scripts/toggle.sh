9313ed6c949f69a91a803b5234bc2d651612d5ec
paskalsq <escfrvrn@proton.me>
2025-09-04 20:27:25 +0300

    Some monitor configuration

---
+++ .config/hypr/scripts/toggle.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+hyprctl keyword monitor "HDMI-A-1, disable"
+hyprctl keyword monitor "DP-2, disable"
+hyprctl keyword monitor "HDMI-A-1, 1280x1024@60,1920x0,1"
+hyprctl keyword monitor "DP-2, 1280x1024@60,-1280x0,1"
