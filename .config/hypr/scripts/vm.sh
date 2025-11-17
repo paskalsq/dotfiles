8dbc658f86cfe45f131db1ea4d88f7859667ac81
paskalsq <escfrvrn@proton.me>
2025-11-17 19:22:41 +0300

    debian

--- .config/hypr/scripts/vm.sh
+++ .config/hypr/scripts/vm.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if virsh --connect qemu:///system list --name --state-running | grep -q "^archlinux$"; then
-  exec virt-viewer --connect qemu:///system archlinux
+if virsh --connect qemu:///system list --name --state-running | grep -q "^debian13$"; then
+  exec virt-viewer --connect qemu:///system debian13
 else
-  notify-send "QEMU/KVM" "Starting the 'archlinux' virtual machine..."
-  virsh --connect qemu:///system start archlinux > /dev/null 2>&1
+  notify-send "QEMU/KVM" "Starting the 'debian13' virtual machine..."
+  virsh --connect qemu:///system start debian13 > /dev/null 2>&1
   sleep 5
-  exec virt-viewer --connect qemu:///system archlinux
+  exec virt-viewer --connect qemu:///system debian13
 fi
